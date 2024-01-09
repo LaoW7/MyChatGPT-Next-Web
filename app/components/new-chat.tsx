67ba9ca0a3aff85e13035d477f9ae240860c6a10
Kay John <KAYWANG0407@ICLOUD.COM>
2024-01-09 15:34:16 +0800

    删除了添加Prompt的花哨动画

--- app/components/new-chat.tsx
+++ app/components/new-chat.tsx
@@ -167,7 +167,7 @@ export function NewChat() {
       </div>
 
       <div className={styles["masks"]} ref={maskRef}>
-        {groups.map((masks, i) => (
+        {/* {groups.map((masks, i) => (
           <div key={i} className={styles["mask-row"]}>
             {masks.map((mask, index) => (
               <MaskItem
@@ -177,7 +177,7 @@ export function NewChat() {
               />
             ))}
           </div>
-        ))}
+        ))} */}
       </div>
     </div>
   );
