911872fbffb2befdfcce541372339580ca4184e6
Kay John <KAYWANG0407@ICLOUD.COM>
2024-01-09 15:04:26 +0800

    减弱了动画视觉效果

--- app/store/config.ts
+++ app/store/config.ts
@@ -42,7 +42,7 @@ export const DEFAULT_CONFIG = {
   disablePromptHint: false,
 
   dontShowMaskSplashScreen: false, // dont show splash screen when create chat
-  hideBuiltinMasks: false, // dont add builtin masks
+  hideBuiltinMasks: true, // dont add builtin masks
 
   customModels: "",
   models: DEFAULT_MODELS as any as LLMModel[],
@@ -146,7 +146,7 @@ export const useAppConfig = createPersistStore(
         state.modelConfig.top_p = 1;
         state.modelConfig.template = DEFAULT_INPUT_TEMPLATE;
         state.dontShowMaskSplashScreen = false;
-        state.hideBuiltinMasks = false;
+        state.hideBuiltinMasks = true;
       }
 
       if (version < 3.5) {
